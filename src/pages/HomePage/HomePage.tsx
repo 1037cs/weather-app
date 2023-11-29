551a106222f22f748c42b2530a747b71acc219ed
Andrey <1037cs@gmail.com>
2023-11-29 23:13:48 +0400

    display mock weather

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from "react";
 import { MainLayout } from "../../layouts/MainLayout";
 import { Header } from "./modules/Header";
+import { DisplayWeather } from "./modules/DisplayWeather";
 
 const HomePage: FC = () => {
   return (
     <MainLayout>
       <Header />
+      <DisplayWeather/>
     </MainLayout>
   );
 };
